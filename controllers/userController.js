19ebc47b02b3457d83679079fd4a0ef90f24bce8
marjuuno <mmapa@gbox.adnu.edu.ph>
2025-11-29 23:44:22 +0800

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,16 +4,46 @@ export const getUser = async (req, res) => {
     try {
         const users = await userService.getAllUsers();
         res.json(users);
-    } catch (error){
-        res.status(500).json(({message: error.message}));
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
-}
+};
+
+export const getUserById = async (req, res) => {
+    try {
+        const user = await userService.getUserById(req.params.id);
+        if (!user) return res.status(404).json({ message: "User not found" });
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
 
 export const createUser = async (req, res) => {
     try {
         const user = await userService.createUser(req.body);
         res.status(201).json(user);
-    } catch (error){
-        res.status(400).json(({message: error.message}));
+    } catch (error) {
+        res.status(400).json({ message: error.message });
     }
-}
+};
+
+export const updateUser = async (req, res) => {
+    try {
+        const user = await userService.updateUser(req.params.id, req.body);
+        if (!user) return res.status(404).json({ message: "User not found" });
+        res.json(user);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+export const deleteUser = async (req, res) => {
+    try {
+        const user = await userService.deleteUser(req.params.id);
+        if (!user) return res.status(404).json({ message: "User not found" });
+        res.json({ message: "User deleted" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
--- services/userService.js
+++ services/userService.js
@@ -2,11 +2,29 @@ import User from "../models/User.js";
 
 const getAllUsers = async () => {
     return await User.find();
-}
+};
+
+const getUserById = async (id) => {
+    return await User.findById(id);
+};
 
 const createUser = async (data) => {
     const user = new User(data);
     return await user.save();
 };
 
-export default { getAllUsers, createUser };
+const updateUser = async (id, data) => {
+    return await User.findByIdAndUpdate(id, data, { new: true });
+};
+
+const deleteUser = async (id) => {
+    return await User.findByIdAndDelete(id);
+};
+
+export default {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+};
