e3aea47506d74d2b1ba3a66e28e448c80f54dea0
marjuuno <mmapa@gbox.adnu.edu.ph>
2025-11-29 22:33:57 +0800

    update user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,29 @@
 import express from "express";
-import { getUser, createUser } from "../controllers/userController.js";
+import {
+    getUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    getUserById
+} from "../controllers/userController.js";
 import { validateUser } from "../validators/userValidator.js";
 
 const router = express.Router();
 
+// GET all users
 router.get("/", getUser);
+
+// GET one user
+router.get("/:id", getUserById);
+
+// CREATE user
 router.post("/", validateUser, createUser);
+
+// UPDATE (PUT or PATCH)
+router.put("/:id", updateUser);
+router.patch("/:id", updateUser);
+
+// DELETE user
+router.delete("/:id", deleteUser);
 
 export default router;
